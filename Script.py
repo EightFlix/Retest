9648f05c4b124ae42241675bf2542b3904d64a6d
𝕊ℙ𝕐𝔻𝔼ℝ <162447832+JoneySinx@users.noreply.github.com>
2025-12-19 19:27:13 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,129 +1,49 @@
 class script(object):
 
-    START_TXT = """<b>ʜᴇʏ {}, <i>{}</i>
+    START_TXT = """<b>ʜᴇʏ {}, <i>{}</i><br>    <br>ɪ ᴀᴍ ᴘᴏᴡᴇʀғᴜʟ ᴀᴜᴛᴏ ғɪʟᴛᴇʀ ᴡɪᴛʜ ʟɪɴᴋ sʜᴏʀᴛᴇɴᴇʀ ʙᴏᴛ. ʏᴏᴜ ᴄᴀɴ ᴜꜱᴇ ᴀꜱ ᴀᴜᴛᴏ ғɪʟᴛᴇʀ ᴡɪᴛʜ ʟɪɴᴋ sʜᴏʀᴛᴇɴᴇʀ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ... ɪᴛ'ꜱ ᴇᴀꜱʏ ᴛᴏ ᴜꜱᴇ ᴊᴜsᴛ ᴀᴅᴅ ᴍᴇ ᴀꜱ ᴀᴅᴍɪɴ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ɪ ᴡɪʟʟ ᴘʀᴏᴠɪᴅᴇ ᴛʜᴇʀᴇ ᴍᴏᴠɪᴇꜱ ᴡɪᴛʜ ʏᴏᴜʀ ʟɪɴᴋ ꜱʜᴏʀᴛᴇɴᴇʀ... ♻️</b>"""
+
+    MY_ABOUT_TXT = """★ Server: <a href=https://www.heroku.com>Heroku</a><br>★ Database: <a href=https://www.mongodb.com>MongoDB</a><br>★ Language: <a href=https://www.python.org>Python</a><br>★ Library: <a href=https://t.me/HydrogramNews>Hydrogram</a>"""
+
+    MY_OWNER_TXT = """★ Name: HA Bots<br>★ Username: @HA_Bots<br>★ Country: Sri Lanka 🇱🇰"""
+
+    STATUS_TXT = """👤 Total Users: <code>{}</code><br>😎 Premium Users: <code>{}</code><br>👥 Total Chats: <code>{}</code><br>🗳 Data database used: <code>{}</code><br><br>🗂 1st database Files: <code>{}</code><br>🗳 1st files database used: <code>{}</code><br><br>🗂 2nd database Files: <code>{}</code><br>🗳 2nd files database used: <code>{}</code><br><br>🚀 Bot Uptime: <code>{}</code>"""
+
+    NEW_GROUP_TXT = """#NewGroup<br>Title - {}<br>ID - <code>{}</code><br>Username - {}<br>Total - <code>{}</code>"""
+
+    NEW_USER_TXT = """#NewUser<br>★ Name: {}<br>★ ID: <code>{}</code>"""
+
+    NOT_FILE_TXT = """👋 Hello {},<br><br>I can't find the <b>{}</b> in my database! 🥲<br><br>👉 Google Search and check your spelling is correct.<br>👉 Please read the Instructions to get better results.<br>👉 Or not been released yet."""
     
-ɪ ᴀᴍ ᴘᴏᴡᴇʀғᴜʟ ᴀᴜᴛᴏ ғɪʟᴛᴇʀ ᴡɪᴛʜ ʟɪɴᴋ sʜᴏʀᴛᴇɴᴇʀ ʙᴏᴛ. ʏᴏᴜ ᴄᴀɴ ᴜꜱᴇ ᴀꜱ ᴀᴜᴛᴏ ғɪʟᴛᴇʀ ᴡɪᴛʜ ʟɪɴᴋ sʜᴏʀᴛᴇɴᴇʀ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ... ɪᴛ'ꜱ ᴇᴀꜱʏ ᴛᴏ ᴜꜱᴇ ᴊᴜsᴛ ᴀᴅᴅ ᴍᴇ ᴀꜱ ᴀᴅᴍɪɴ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ɪ ᴡɪʟʟ ᴘʀᴏᴠɪᴅᴇ ᴛʜᴇʀᴇ ᴍᴏᴠɪᴇꜱ ᴡɪᴛʜ ʏᴏᴜʀ ʟɪɴᴋ ꜱʜᴏʀᴛᴇɴᴇʀ... ♻️</b>"""
+    IMDB_TEMPLATE = """✅ I Found: <code>{query}</code><br><br>🏷 Title: <a href={url}>{title}</a><br>🎭 Genres: {genres}<br>📆 Year: <a href={url}/releaseinfo>{year}</a><br>🌟 Rating: <a href={url}/ratings>{rating} / 10</a><br>☀️ Languages: {languages}<br>📀 RunTime: {runtime} Minutes<br><br>🗣 Requested by: {message.from_user.mention}<br>©️ Powered by: <b>{message.chat.title}</b>"""
 
-    MY_ABOUT_TXT = """★ Server: <a href=https://www.heroku.com>Heroku</a>
-★ Database: <a href=https://www.mongodb.com>MongoDB</a>
-★ Language: <a href=https://www.python.org>Python</a>
-★ Library: <a href=https://t.me/HydrogramNews>Hydrogram</a>"""
-
-    MY_OWNER_TXT = """★ Name: HA Bots
-★ Username: @HA_Bots
-★ Country: Sri Lanka 🇱🇰"""
-
-    STATUS_TXT = """👤 Total Users: <code>{}</code>
-😎 Premium Users: <code>{}</code>
-👥 Total Chats: <code>{}</code>
-🗳 Data database used: <code>{}</code>
-
-🗂 1st database Files: <code>{}</code>
-🗳 1st files database used: <code>{}</code>
-
-🗂 2nd database Files: <code>{}</code>
-🗳 2nd files database used: <code>{}</code>
-
-🚀 Bot Uptime: <code>{}</code>"""
-
-    NEW_GROUP_TXT = """#NewGroup
-Title - {}
-ID - <code>{}</code>
-Username - {}
-Total - <code>{}</code>"""
-
-    NEW_USER_TXT = """#NewUser
-★ Name: {}
-★ ID: <code>{}</code>"""
-
-    NOT_FILE_TXT = """👋 Hello {},
-
-I can't find the <b>{}</b> in my database! 🥲
-
-👉 Google Search and check your spelling is correct.
-👉 Please read the Instructions to get better results.
-👉 Or not been released yet."""
-    
-    IMDB_TEMPLATE = """✅ I Found: <code>{query}</code>
-
-🏷 Title: <a href={url}>{title}</a>
-🎭 Genres: {genres}
-📆 Year: <a href={url}/releaseinfo>{year}</a>
-🌟 Rating: <a href={url}/ratings>{rating} / 10</a>
-☀️ Languages: {languages}
-📀 RunTime: {runtime} Minutes
-
-🗣 Requested by: {message.from_user.mention}
-©️ Powered by: <b>{message.chat.title}</b>"""
-
-    FILE_CAPTION = """<i>{file_name}</i>
-
-🚫 ᴘʟᴇᴀsᴇ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ᴄʟᴏsᴇ ʙᴜᴛᴛᴏɴ ɪꜰ ʏᴏᴜ ʜᴀᴠᴇ sᴇᴇɴ ᴛʜᴇ ᴍᴏᴠɪᴇ 🚫"""
+    FILE_CAPTION = """<i>{file_name}</i><br><br>🚫 ᴘʟᴇᴀsᴇ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ᴄʟᴏsᴇ ʙᴜᴛᴛᴏɴ ɪꜰ ʏᴏᴜ ʜᴀᴠᴇ sᴇᴇɴ ᴛʜᴇ ᴍᴏᴠɪᴇ 🚫"""
 
     WELCOME_TEXT = """👋 Hello {mention}, Welcome to {title} group! 💞"""
 
-    HELP_TXT = """👋 Hello {},
+    HELP_TXT = """👋 Hello {},<br>    <br>I can filter movie and series you want<br>Just type you want movie or series in my PM or adding me in to group<br>And i have more feature for you<br>Just try my commands"""
+
+    ADMIN_COMMAND_TXT = """<b>Here is bot admin commands 👇<br><br><br>/index_channels - to check how many index channel id added<br>/stats - to get bot status<br>/delete - to delete files using query<br>/delete_all - to delete all indexed file<br>/broadcast - to send message to all bot users<br>/grp_broadcast - to send message to all groups<br>/pin_broadcast - to send message as pin to all bot users.<br>/pin_grp_broadcast - to send message as pin to all groups.<br>/restart - to restart bot<br>/leave - to leave your bot from particular group<br>/users - to get all users details<br>/chats - to get all groups<br>/invite_link - to generate invite link<br>/index - to index bot accessible channels<br>/add_prm - to add new premium user<br>/rm_prm - to remove premium user<br>/delreq - to delete join request in db<br>/set_req_fsub - to set request force subscribe channel<br>/set_fsub - to set force subscribe channels</b>"""
     
-I can filter movie and series you want
-Just type you want movie or series in my PM or adding me in to group
-And i have more feature for you
-Just try my commands"""
+    PLAN_TXT = """Activate any premium plan to get exclusive features.<br><br>You can activate any premium plan and then you can get exclusive features.<br><br>- INR {} for pre day -<br><br>Basic premium features:<br>Ad free experience<br>Online watch and fast download<br>No need join channels<br>No need verify<br>No shortlink<br>Admins support<br>And more...<br><br>Support: {}"""
 
-    ADMIN_COMMAND_TXT = """<b>Here is bot admin commands 👇
+    USER_COMMAND_TXT = """<b>यहाँ बॉट के एडवांस टूल्स और कमांड्स हैं 👇</b>
 
+<b>🖼️ Permanent Links (हमेशा के लिए):</b>
+• /graph - Graph.org (Max 5MB - बेस्ट फॉर इमेजेज)
+• /ct - Catbox (Max 200MB - बेस्ट फॉर फाइल्स)
 
-/index_channels - to check how many index channel id added
-/stats - to get bot status
-/delete - to delete files using query
-/delete_all - to delete all indexed file
-/broadcast - to send message to all bot users
-/grp_broadcast - to send message to all groups
-/pin_broadcast - to send message as pin to all bot users.
-/pin_grp_broadcast - to send message as pin to all groups.
-/restart - to restart bot
-/leave - to leave your bot from particular group
-/users - to get all users details
-/chats - to get all groups
-/invite_link - to generate invite link
-/index - to index bot accessible channels
-/add_prm - to add new premium user
-/rm_prm - to add remove premium user
-/delreq - to delete join request in db (if change REQUEST_FORCE_SUB_CHANNELS using /set_req_fsub then must need use this command)
-/set_req_fsub - to set request force subscribe channel
-/set_fsub - to set force subscribe channels</b>"""
+<b>⏳ Temporary Links (समय सीमा के साथ):</b>
+• /lt - Litterbox (24 घंटे बाद डिलीट, 1GB लिमिट)
+• /go - GoFile (24 घंटे बाद डिलीट, कोई साइज लिमिट नहीं)
+• /trans - Transfer.sh (14 दिन तक वैध, 10GB लिमिट)
+• /img_2_link - Uguu.se (24 घंटे बाद डिलीट, 100MB लिमिट)
+
+<b>⚙️ अन्य कमांड्स:</b>
+• /start - बॉट की स्थिति जांचें
+• /myplan - अपना प्रीमियम प्लान देखें
+• /plan - प्रीमियम प्लान की जानकारी
+• /settings - ग्रुप सेटिंग्स बदलें
+• /connect - ग्रुप को PM से जोड़ें
+• /id - चैट या फाइल की ID प्राप्त करें</b>"""
     
-    PLAN_TXT = """Activate any premium plan to get exclusive features.
-
-You can activate any premium plan and then you can get exclusive features.
-
-- INR {} for pre day -
-
-Basic premium features:
-Ad free experience
-Online watch and fast download
-No need joind channels
-No need verify
-No shortlink
-Admins support
-And more...
-
-Support: {}"""
-
-    USER_COMMAND_TXT = """<b>Here is bot user commands 👇
-
-/start - to check bot alive or not
-/myplan - to check my activated premium plan
-/plan - to view premium plan details
-/img_2_link - upload image to uguu.se and get link
-/settings - to change group settings as your wish
-/connect - to connect group settings to PM
-/id - to check group or channel id</b>"""
-    
-    SOURCE_TXT = """<b>ʙᴏᴛ ɢɪᴛʜᴜʙ ʀᴇᴘᴏsɪᴛᴏʀʏ -
-
-- ᴛʜɪꜱ ʙᴏᴛ ɪꜱ ᴀɴ ᴏᴘᴇɴ ꜱᴏᴜʀᴄᴇ ᴘʀᴏᴊᴇᴄᴛ.
-
-- ꜱᴏᴜʀᴄᴇ - <a href=https://github.com/HA-Bots/Auto-Filter-Bot>ʜᴇʀᴇ</a>
-
-- ᴅᴇᴠʟᴏᴘᴇʀ - @HA_Bots"""
-
+    SOURCE_TXT = """<b>ʙᴏᴛ ɢɪᴛʜᴜʙ ʀᴇᴘᴏsɪᴛᴏʀʏ -<br><br>- ᴛʜɪꜱ ʙᴏᴛ ɪꜱ ᴀɴ ᴏᴘᴇɴ ꜱᴏᴜʀᴄᴇ ᴘʀᴏᴊᴇᴄᴛ.<br><br>- ꜱᴏᴜʀᴄᴇ - <a href=https://github.com/HA-Bots/Auto-Filter-Bot>ʜᴇʀᴇ</a><br><br>- ᴅᴇᴠʟᴏᴘᴇʀ - @HA_Bots"""
